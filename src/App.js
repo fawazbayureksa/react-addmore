e8b74eb15353ddfd32744d1010a0f26ce2583a13
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-04-16 21:48:37 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
-function App() {
+export default function App() {
+  const [inputList, setInputList] = useState([
+    {
+      firstName: '',
+      lastName: '',
+    },
+  ]);
+
+  const handleChange = (e, index) => {
+    // mengambil name dan value dari target yang onChange
+    const { name, value } = e.target;
+
+    const list = [...inputList];
+    list[index][name] = value;
+
+    setInputList(list);
+  };
+
+  const hanleAddInput = () => {
+    setInputList([...inputList, { firstName: '', lastName: '' }]);
+
+
+    // const list = [...inputList];
+    // list.push({ firstName: '', lastName: '' });
+    // setInputList(list);
+  
+  
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Membuat tombol Add More dengan Reac JS</h1>
+      </div>
+
+      {inputList.map((item, i) => {
+        return (
+          <div key={i} className="flex">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={item.firstName}
+              onChange={(e) => handleChange(e, i)}
+            />
+
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              value={item.lastName}
+              onChange={(e) => handleChange(e, i)}
+            />
+            <input
+              type="button"
+              value="Add"
+              className="button"
+              onClick={hanleAddInput}
+            />
+          </div>
+        );
+      })}
+      <pre>{JSON.stringify(inputList, null, 2)}</pre>
+    </>
   );
 }
-
-export default App;
